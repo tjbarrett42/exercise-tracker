84959c81b38c4c17ae7de9a58e43199c858dcd1a
tjbarrett42 <tjbarrett42@gmail.com>
2020-11-18 20:41:04 -0500

    Navbar setup

---
+++ mern-exercise-tracker/src/components/exercise-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+  render () {
+    return (
+      <div>
+        <p>Exercises list component, ur at it</p>
+      </div>
+    );
+  }
+}
